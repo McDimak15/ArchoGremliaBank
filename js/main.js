b124f7a92b08617827cd6215dd4eeae6ffbb334b
McDimak15 <59174406+McDimak15@users.noreply.github.com>
2022-05-27 12:57:41 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function getInfo() {
+	var username = document.getElementById("name").value
+	var password = document.getElementById("password").value
+	if(username == "Dima" && password == "D01") {
+		alert("Твій баланс: банк архо-гремлії")
+	}
+	if(username == "Artem" && password == "A01") {
+		alert("Твій баланс: банк архо-гремлії")
+	}
+	if(username == "Maks" && password == "M02") {
+		alert("Твій баланс: 10 ортоків")
+	}
+	if(username == "David" && password == "D02") {
+		alert("Твій баланс: 10 ортоків")
+	}
+	if(username == "Stas" && password == "S03") {
+		alert("Твій баланс: 10 ортоків")
+	}
+	if(username == "Zahar" && password == "Z03") {
+		alert("Твій баланс: 10 ортоків")
+	}
+}
