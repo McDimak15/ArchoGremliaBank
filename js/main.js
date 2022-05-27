ff98927fb053e2311c6c4a1bc5af0550dae0eeb3
McDimak15 <59174406+McDimak15@users.noreply.github.com>
2022-05-27 13:28:30 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 function getInfo() {
 	var username = document.getElementById("name").value
 	var password = document.getElementById("password").value
+	var currencyoutput = document.getElementById("CurrencyOutput")
 	if(username == "Dima" && password == "D01") {
-		alert("Твій баланс: банк архо-гремлії")
+		currencyoutput.value = "Твій баланс: банк архо-гремлії"
 	}
 	if(username == "Artem" && password == "A01") {
-		alert("Твій баланс: банк архо-гремлії")
+			currencyoutput.value = "Твій баланс: банк архо-гремлії"
 	}
 	if(username == "Maks" && password == "M02") {
-		alert("Твій баланс: 10 ортоків")
+			currencyoutput.value = "Твій баланс: 10 ортоків"
 	}
 	if(username == "David" && password == "D02") {
-		alert("Твій баланс: 10 ортоків")
+			currencyoutput.value = "Твій баланс: 10 ортоків"
 	}
 	if(username == "Stas" && password == "S03") {
-		alert("Твій баланс: 10 ортоків")
+			currencyoutput.value = "Твій баланс: 10 ортоків"
 	}
 	if(username == "Zahar" && password == "Z03") {
-		alert("Твій баланс: 10 ортоків")
+			currencyoutput.value = "Твій баланс: 10 ортоків"
 	}
 }
